de8788947b61169708385adeefbf2cd936e6297c
David Chaves <davi.ch.fe@gmail.com>
2020-09-28 20:48:14 -0300

    Upload project

---
+++ src/main/java/com/chaves/manageemployeeapi/ManageEmployeeApiApplication.java
@@ -0,0 +1,13 @@
+package com.chaves.manageemployeeapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ManageEmployeeApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ManageEmployeeApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/chaves/manageemployeeapi/ManageEmployeeApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chaves.manageemployeeapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ManageEmployeeApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
